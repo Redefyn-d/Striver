e18a142e3d30030175f2a31995f5c03a798bd985
Lokesh Verma <148458558+Redefyn-d@users.noreply.github.com>
2026-05-07 05:48:37 +0530

    Implement number reversal in reverseNum.java

---
+++ reverseNum.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int newn = 0;
+        while(n!=0){
+            newn = (newn*10)+(n%10);
+            n/=10;
+        }
+        System.out.print(newn);
+    }
+}
