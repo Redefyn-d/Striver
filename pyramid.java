ef469b3e7c6a2886be49a7ac0932d1cbe3e160d7
Lokesh Verma <148458558+Redefyn-d@users.noreply.github.com>
2026-05-08 05:47:40 +0530

    Update pyramid.java

--- pyramid.java
+++ pyramid.java
@@ -30,3 +30,23 @@ public class Main{
         }
     }
 }
+//Diff Pyramid 
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int k = 0;k<2*i+1;k++){
+                System.out.print("*");
+            }
+            for(int l = 0;l<n-i-1;l++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
