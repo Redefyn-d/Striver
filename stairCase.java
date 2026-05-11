e1936d87e98747e9edd30d3cd62c0d560ccf5d1d
Lokesh Verma <148458558+Redefyn-d@users.noreply.github.com>
2026-05-12 00:21:53 +0530

    Update stairCase.java

--- stairCase.java
+++ stairCase.java
@@ -26,3 +26,19 @@ public class Main{
         }
     }
 }
+
+//pattern 14 
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0;i<=n;i++){
+            char val = 'A';
+            for(int j = 0;j<i;j++){
+                System.out.print(val);
+                val++;
+            }
+            System.out.println();
+        }
+    }
+}
